96f8ae13c39fe7f2fe53d6d899e2561e5ca84b08
sharan <sharan@rpanp.com>
2026-01-26 22:03:40 +0545

    refactor: database wise typeorm migrations

--- sg-cli/src/new-app-generator/tsconfig.generator.js
+++ sg-cli/src/new-app-generator/tsconfig.generator.js
@@ -6,10 +6,14 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export async function generateTsConfig(appName) {
+export async function generateTsConfig(appName, dbName) {
   const root = path.join(process.cwd(), appName, "tsconfig.json");
-  const contents = await ejs.renderFile(
-    path.join(__dirname, "../../templates/app/tsconfig.json.ejs"),
-  );
+  const contents = dbName
+    ? await ejs.renderFile(
+        path.join(__dirname, "../../templates/app/typeorm-tsconfig.json.ejs"),
+      )
+    : await ejs.renderFile(
+        path.join(__dirname, "../../templates/app/tsconfig.json.ejs"),
+      );
   await writeFile(root, contents);
 }
